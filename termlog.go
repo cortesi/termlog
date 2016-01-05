8411c4feacf5daac0b5ca877e53b6c560fa3d409
Aldo Cortesi <aldo@nullcube.com>
2016-01-06 11:48:21 +1300

    Refactor internals to make formatting more flexible

--- termlog.go
+++ termlog.go
@@ -3,6 +3,7 @@
 package termlog
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"sync"
@@ -11,6 +12,13 @@ import (
 	"github.com/fatih/color"
 	"golang.org/x/crypto/ssh/terminal"
 	"golang.org/x/net/context"
+)
+
+const (
+	say = iota
+	notice
+	warn
+	shout
 )
 
 const defaultTimeFmt = "15:04:05: "
@@ -54,7 +62,8 @@ type Logger interface {
 	ShoutAs(name string, format string, args ...interface{})
 }
 
-// Group is a collected group of log entries
+// Group is a collected group of log entries. Logs are only displayed once the
+// Done method is called.
 type Group interface {
 	Logger
 	Done()
@@ -69,10 +78,8 @@ type TermLog interface {
 }
 
 type line struct {
-	name   string
-	color  *color.Color
-	format string
-	args   []interface{}
+	name string
+	str  string
 }
 
 // Log is the top-level log structure
@@ -96,6 +103,30 @@ func NewLog() *Log {
 		l.Color(false)
 	}
 	return l
+}
+
+func (l *Log) format(timestamp bool, level int, format string, args []interface{}) string {
+	ts := ""
+	if timestamp {
+		f := l.Palette.Timestamp.SprintfFunc()
+		ts = f(
+			"%s", time.Now().Format(l.TimeFmt),
+		)
+	}
+	var p *color.Color
+	switch level {
+	case say:
+		p = l.Palette.Say
+	case notice:
+		p = l.Palette.Notice
+	case warn:
+		p = l.Palette.Warn
+	case shout:
+		p = l.Palette.Shout
+	default:
+		panic("unknown log level")
+	}
+	return ts + p.SprintfFunc()(format, args...)
 }
 
 // Color sets the state of colour output - true to turn on, false to disable.
@@ -122,63 +153,52 @@ func (l *Log) output(quiet bool, lines ...*line) {
 	if len(lines) == 0 {
 		return
 	}
-	first := true
 	for _, line := range lines {
 		if _, ok := l.enabled[line.name]; !ok {
 			continue
 		}
-		var format string
-		if first {
-			l.Palette.Timestamp.Printf(
-				"%s", time.Now().Format(l.TimeFmt),
-			)
-			first = false
-			format = line.format + "\n"
-		} else {
-			format = indent + line.format + "\n"
-		}
-		line.color.Printf(format, line.args...)
+		fmt.Fprintf(color.Output, line.str+"\n")
 	}
 }
 
 // Say logs a line
 func (l *Log) Say(format string, args ...interface{}) {
-	l.output(l.quiet, &line{"", l.Palette.Say, format, args})
+	l.output(l.quiet, &line{"", l.format(true, say, format, args)})
 }
 
 // Notice logs a line with the Notice color
 func (l *Log) Notice(format string, args ...interface{}) {
-	l.output(l.quiet, &line{"", l.Palette.Notice, format, args})
+	l.output(l.quiet, &line{"", l.format(true, notice, format, args)})
 }
 
 // Warn logs a line with the Warn color
 func (l *Log) Warn(format string, args ...interface{}) {
-	l.output(l.quiet, &line{"", l.Palette.Warn, format, args})
+	l.output(l.quiet, &line{"", l.format(true, warn, format, args)})
 }
 
 // Shout logs a line with the Shout color
 func (l *Log) Shout(format string, args ...interface{}) {
-	l.output(l.quiet, &line{"", l.Palette.Shout, format, args})
+	l.output(l.quiet, &line{"", l.format(true, shout, format, args)})
 }
 
 // SayAs logs a line
 func (l *Log) SayAs(name string, format string, args ...interface{}) {
-	l.output(l.quiet, &line{name, l.Palette.Say, format, args})
+	l.output(l.quiet, &line{name, l.format(true, say, format, args)})
 }
 
 // NoticeAs logs a line with the Notice color
 func (l *Log) NoticeAs(name string, format string, args ...interface{}) {
-	l.output(l.quiet, &line{name, l.Palette.Notice, format, args})
+	l.output(l.quiet, &line{name, l.format(true, notice, format, args)})
 }
 
 // WarnAs logs a line with the Warn color
 func (l *Log) WarnAs(name string, format string, args ...interface{}) {
-	l.output(l.quiet, &line{name, l.Palette.Warn, format, args})
+	l.output(l.quiet, &line{name, l.format(true, warn, format, args)})
 }
 
 // ShoutAs logs a line with the Shout color
 func (l *Log) ShoutAs(name string, format string, args ...interface{}) {
-	l.output(l.quiet, &line{name, l.Palette.Shout, format, args})
+	l.output(l.quiet, &line{name, l.format(true, shout, format, args)})
 }
 
 // Group creates a new log group
@@ -189,69 +209,6 @@ func (l *Log) Group() Group {
 		log:     l,
 		quiet:   l.quiet,
 	}
-}
-
-// Group is a group of lines that constitue a single log entry that won't be
-// split. Lines in a group are indented.
-type group struct {
-	palette *Palette
-	lines   []*line
-	log     *Log
-	quiet   bool
-}
-
-func (g *group) addLine(name string, color *color.Color, format string, args []interface{}) {
-	g.lines = append(g.lines, &line{name, color, format, args})
-}
-
-// Say logs a line
-func (g *group) Say(format string, args ...interface{}) {
-	g.addLine("", g.palette.Say, format, args)
-}
-
-// Notice logs a line with the Notice color
-func (g *group) Notice(format string, args ...interface{}) {
-	g.addLine("", g.palette.Notice, format, args)
-}
-
-// Warn logs a line with the Warn color
-func (g *group) Warn(format string, args ...interface{}) {
-	g.addLine("", g.palette.Warn, format, args)
-}
-
-// Shout logs a line with the Shout color
-func (g *group) Shout(format string, args ...interface{}) {
-	g.addLine("", g.palette.Shout, format, args)
-}
-
-// SayAs logs a line
-func (g *group) SayAs(name string, format string, args ...interface{}) {
-	g.addLine(name, g.palette.Say, format, args)
-}
-
-// NoticeAs logs a line with the Notice color
-func (g *group) NoticeAs(name string, format string, args ...interface{}) {
-	g.addLine(name, g.palette.Notice, format, args)
-}
-
-// WarnAs logs a line with the Warn color
-func (g *group) WarnAs(name string, format string, args ...interface{}) {
-	g.addLine(name, g.palette.Warn, format, args)
-}
-
-// ShoutAs logs a line with the Shout color
-func (g *group) ShoutAs(name string, format string, args ...interface{}) {
-	g.addLine(name, g.palette.Shout, format, args)
-}
-
-// Done outputs the group to screen
-func (g *group) Done() {
-	g.log.output(g.quiet, g.lines...)
-}
-
-// Quiet disables output for this subgroup
-func (g *group) Quiet() {
-	g.quiet = true
 }
 
 // NewContext creates a new context with an included Logger
